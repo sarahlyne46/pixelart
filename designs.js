07d250dfe99255baacb0cac4f97d3c0cae5e3cc0
sarahlyne46 <86782977+sarahlyne46@users.noreply.github.com>
2021-07-07 17:42:08 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,10 @@
 // Select color input
 // Select size input
-const size = document.getElementById('sizePicker');
-const color = document.getElementById("colorPicker");
+var size = document.getElementById('sizePicker');
+var color = document.getElementById("colorPicker");
 
 // identifying the submit button
-const submit = document.getElementById('submit');
+var submit = document.getElementById('submit');
 
 // When size is submitted by the user, call makeGrid()
 submit.addEventListener('click', function(height, width){
@@ -14,16 +14,16 @@ submit.addEventListener('click', function(height, width){
 function makeGrid(event) {
   	event.preventDefault();
   	//table, height and width variables declared to create the pixel canvas
-	const table = document.getElementById('pixelCanvas');
-	const height = document.getElementById('inputHeight').value;
-	const width = document.getElementById('inputWidth').value;
+	var table = document.getElementById('pixelCanvas');
+	var height = document.getElementById('inputHeight').value;
+	var width = document.getElementById('inputWidth').value;
 	//creating a blank table in the HTML
   	table.innerHTML = "";
   	//build table depending on values entered by the user.  Reviewed the knowledge area to get the table to build.
-  	for (const r = 1; r < height; r++) {
-    	const row = document.createElement("tr");
-    	for (const c = 0; c < width; c++) {
-      		const cell = document.createElement("td");
+  	for (let r = 1; r < height; r++) {
+    	let row = document.createElement("tr");
+    	for (let c = 0; c < width; c++) {
+      		let cell = document.createElement("td");
       		row.appendChild(cell); //append the column to the table
       		cell.addEventListener("click", function (cellColor) {
         		cellColor.target.style.backgroundColor = color.value;  //set background color to user selection.
